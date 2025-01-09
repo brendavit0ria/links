1851048f0a2f129aaa89e4071c61981e0ae9dcc8
brendavit0ria <brendavitoria0602@gmail.com>
2025-01-09 09:20:42 -0300

    feat: using modal

--- src/app/app/app.tsx
+++ src/app/app/app.tsx
@@ -1,4 +1,11 @@
-import { View, Image, TouchableOpacity, FlatList } from "react-native";
+import {
+  View,
+  Image,
+  TouchableOpacity,
+  FlatList,
+  Modal,
+  Text,
+} from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 
 import { styles } from "./styles";
@@ -34,6 +41,27 @@ export default function App() {
         contentContainerStyle={styles.linksContent}
         showsVerticalScrollIndicator={false}
       />
+
+      <Modal transparent visible>
+        <View style={styles.modal}>
+          <View style={styles.modalContent}>
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalCategory}>Curso</Text>
+              <TouchableOpacity>
+                <MaterialIcons
+                  name="close"
+                  size={20}
+                  color={colors.gray[400]}
+                />
+              </TouchableOpacity>
+            </View>
+
+            <Text style={styles.modalLinkName}>Google</Text>
+
+            <Text style={styles.modalUrl}>https://www.google.com/</Text>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
