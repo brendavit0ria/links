bc32201595ece852c3ebc38b36043b5de9c17abe
brendavit0ria <brendavitoria0602@gmail.com>
2025-01-11 11:43:37 -0300

    feat: creating states

--- src/app/Add/Add.tsx
+++ src/app/Add/Add.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { View, Text, TouchableOpacity } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { router } from "expo-router";
@@ -10,6 +11,13 @@ import { Input } from "@/components/Input/Input";
 import { Button } from "@/components/Button/Button";
 
 export default function Add() {
+  const [name, setName] = useState("");
+  const [url, setUrl] = useState("");
+
+  function handleAdd() {
+    console.log({ name, url });
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -24,9 +32,9 @@ export default function Add() {
       <Categories />
 
       <View style={styles.form}>
-        <Input placeholder="Nome" onChangeText={console.log} />
-        <Input placeholder="Url" />
-        <Button title="Adicionar" />
+        <Input placeholder="Nome" onChangeText={setName} autoCorrect={false} />
+        <Input placeholder="Url" onChangeText={setUrl} autoCorrect={false} />
+        <Button title="Adicionar" onPress={handleAdd} />
       </View>
     </View>
   );
