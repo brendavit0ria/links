35850ffb4ff5a26c3050abf6aa085b1b9ac0f08b
brendavit0ria <brendavitoria0602@gmail.com>
2025-01-12 13:15:25 -0300

    feat: validating data

--- src/app/Add/Add.tsx
+++ src/app/Add/Add.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, Alert } from "react-native";
 import { MaterialIcons } from "@expo/vector-icons";
 import { router } from "expo-router";
 
@@ -16,6 +16,18 @@ export default function Add() {
   const [url, setUrl] = useState("");
 
   function handleAdd() {
+    if (!category) {
+      return Alert.alert("Categoria", "Selecione a categoria");
+    }
+
+    if (!name.trim()) {
+      return Alert.alert("Nome", "Informe o nome");
+    }
+
+    if (!url.trim()) {
+      return Alert.alert("URL", "Informe a URL");
+    }
+
     console.log({ name, url });
   }
 
@@ -34,7 +46,7 @@ export default function Add() {
 
       <View style={styles.form}>
         <Input placeholder="Nome" onChangeText={setName} autoCorrect={false} />
-        <Input placeholder="Url" onChangeText={setUrl} autoCorrect={false} />
+        <Input placeholder="URL" onChangeText={setUrl} autoCorrect={false} />
         <Button title="Adicionar" onPress={handleAdd} />
       </View>
     </View>
