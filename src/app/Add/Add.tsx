982e5cbb5f3d9359c0ddf537c730608a70bff0d0
brendavit0ria <brendavitoria0602@gmail.com>
2025-01-14 09:34:59 -0300

    feat: add and list links

--- src/app/Add/Add.tsx
+++ src/app/Add/Add.tsx
@@ -5,6 +5,7 @@ import { router } from "expo-router";
 
 import { styles } from "./styles";
 import { colors } from "@/styles/colors";
+import { linkStorage } from "@/storage/link-storage";
 
 import { Categories } from "@/components/Categories/Categories";
 import { Input } from "@/components/Input/Input";
@@ -15,20 +16,33 @@ export default function Add() {
   const [name, setName] = useState("");
   const [url, setUrl] = useState("");
 
-  function handleAdd() {
-    if (!category) {
-      return Alert.alert("Categoria", "Selecione a categoria");
-    }
+  async function handleAdd() {
+    try {
+      if (!category) {
+        return Alert.alert("Categoria", "Selecione a categoria");
+      }
 
-    if (!name.trim()) {
-      return Alert.alert("Nome", "Informe o nome");
-    }
+      if (!name.trim()) {
+        return Alert.alert("Nome", "Informe o nome");
+      }
 
-    if (!url.trim()) {
-      return Alert.alert("URL", "Informe a URL");
-    }
+      if (!url.trim()) {
+        return Alert.alert("URL", "Informe a URL");
+      }
 
-    console.log({ name, url });
+      await linkStorage.save({
+        id: new Date().getTime().toString(),
+        name,
+        url,
+        category,
+      });
+
+      const data = await linkStorage.get();
+      console.log(data);
+    } catch (error) {
+      Alert.alert("Erro", "Não foi possível salvar o link");
+      console.log(error);
+    }
   }
 
   return (
@@ -46,7 +60,12 @@ export default function Add() {
 
       <View style={styles.form}>
         <Input placeholder="Nome" onChangeText={setName} autoCorrect={false} />
-        <Input placeholder="URL" onChangeText={setUrl} autoCorrect={false} />
+        <Input
+          placeholder="URL"
+          onChangeText={setUrl}
+          autoCorrect={false}
+          autoCapitalize="none"
+        />
         <Button title="Adicionar" onPress={handleAdd} />
       </View>
     </View>
